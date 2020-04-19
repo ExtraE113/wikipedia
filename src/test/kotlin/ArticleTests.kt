eed53c2980a1b1741cf5aadf4499b61451d29d9c
ExtraE113 <ezra@mamans.com>
2020-04-18 23:54:42 -0700

    implemented (mostly...)

--- src/test/kotlin/ArticleTests.kt
+++ src/test/kotlin/ArticleTests.kt
@@ -1,23 +1,36 @@
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldBeIn
 import org.junit.jupiter.api.Test
 
 class ArticleTests {
-    @Test
-    fun ArticleTests() {
-        //fixme [A] implement
-//        val ArticlesHolder =  ArticlesHolder(5)
-//        val articleBuilder = ArticleBuilder()
-//        articleBuilder.title = "abc"
-//        articleBuilder.title shouldBeEqualTo "abc"
-//        articleBuilder.reset()
-//        articleBuilder.title shouldBeEqualTo null
-//        articleBuilder.firstLink = ArticlesHolder["def"]
-//        articleBuilder.title = "ghk"
-//        ArticlesHolder["ghk"] = articleBuilder.build()
-//        articleBuilder.title shouldBeEqualTo null
-//        val targetArticle = Article(ArrayList(), "ghk",false)
-//        targetArticle.firstLink = ArticlesHolder["def"]
-//        articlesHolder["ghk"] shouldBeEqualTo targetArticle
-    }
+	@Test
+	fun ArticleTests() {
+		//todo document and expand
+
+		val articlesHolder = ArticlesHolder(5)
+		val articleBuilder = ArticleBuilder()
+
+		//some super basic tests
+		articleBuilder.title = "abc"
+		articleBuilder.title shouldBeEqualTo "abc"
+		articleBuilder.reset()
+		articleBuilder.title shouldBeEqualTo null
+
+
+		articleBuilder.firstLink = articlesHolder["def"]
+		articleBuilder.title = "ghk"
+        articleBuilder.isDirectlyLinkedToEndArticle = true
+		articlesHolder["ghk"] = articleBuilder.build()
+        //test that reset is working AND called after .build
+		articleBuilder.title shouldBeEqualTo null
+        articleBuilder.firstLink shouldBeEqualTo null
+        articleBuilder.isDirectlyLinkedToEndArticle shouldBeEqualTo false
+
+		val targetArticle = Article(ArrayList(), "ghk", true)
+		targetArticle.firstLink = articlesHolder["def"]
+
+        //test equality and builder NOTE: checks that parents are in the same order for equality, but this isn't strictly-speaking necessary
+		articlesHolder["ghk"] shouldBeEqualTo targetArticle
+
+		//todo test parent code!!
+	}
 }
